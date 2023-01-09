708c20dcd7ceed58b00bac4d5c43b1c91991419e
taty1202 <115900680+taty1202@users.noreply.github.com>
2023-01-09 09:34:16 -0800

    deleted comment

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -14,8 +14,8 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
-    // documentation: https://dictionaryapi.dev/e
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
