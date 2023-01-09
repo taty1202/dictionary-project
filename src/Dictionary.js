9873730cf14da9359a7c225e12eeb7b5f1777df7
taty1202 <115900680+taty1202@users.noreply.github.com>
2023-01-09 08:27:09 -0800

    installed axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+ 
+
   
   function search(event) {
     event.preventDefault();
@@ -13,6 +16,13 @@ export default function Dictionary() {
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
+  function showResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  axios.get(apiUrl).then(showResponse);
   
   return ( 
     <div className="Dictionary">
